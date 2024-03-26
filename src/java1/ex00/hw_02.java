be6b6c36eb11d324a93138137d7cd63a2e31e141
yessh <keh1058@gmail.com>
2024-03-26 20:38:47 +0900

    hi

--- src/java1/ex00/hw_02.java
+++ src/java1/ex00/hw_02.java
@@ -2,9 +2,7 @@ package java1.ex00;
 
 public class hw_02 {
     public static void main(String[] args) {
-        int a; int b;
-        a = 10; b = 5;
-        System.out.printf("%d %d",a,b);
+
     }
 
 
