f5d1c3cdd82c5b2b9e23cabb5b36e8ceb15b4f1a
IkJuLim <limik1004@gmail.com>
2024-03-27 15:09:49 +0900

    ex01

---
+++ src/java1/ex_01/hw_2.java
@@ -0,0 +1,9 @@
+package ex_01;
+
+public class hw_2 {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
